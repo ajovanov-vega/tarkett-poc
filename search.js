2c379b8f22a41bbe365f0987628f3bfabd468f98
Aleksandar Jovanov <a.jovanov@vegait.rs>
2023-10-16 11:54:33 +0200

    Filter logic improvement and other improvements

--- search.js
+++ search.js
@@ -1,84 +1,91 @@
-const recommendations = [
-  {
-    label: "Natural Oak",
-    filters: ["kind-of-wood-oak"],
-  },
-  {
-    label: "Oak Light",
-    filters: ["kind-of-wood-oak", "hue-light"],
-  },
-  {
-    label: "Oak medium",
-    filters: ["kind-of-wood-oak", "hue-medium"],
-  },
-  {
-    label: "Oak light gray",
-    filters: ["kind-of-wood-oak", "hue-light", "pattern-gray"],
-  },
-  {
-    label: "Walnut tres",
-    filters: ["kind-of-wood-walnut"],
-  },
-  {
-    label: "Light Walnut",
-    filters: ["kind-of-wood-walnut", "hue-light"],
-  },
-  {
-    label: "Dark Walnut tres",
-    filters: ["kind-of-wood-walnut", "hue-dark"],
-  },
-  {
-    label: "Wood Flooring",
-    filters: [],
-  },
-  {
-    label: "Laminate Flooring",
-    filters: [],
-  },
-  {
-    label: "Caring for Laminated Flooring",
-    filters: [],
-  },
-  {
-    label: "Vinyl Flooring",
-    filters: [],
-  },
-  {
-    label: "Caring for Vinyl Flooring",
-    filters: [],
-  },
-];
-
 document.addEventListener("DOMContentLoaded", () => {
   const search = document.querySelector(".js-search-form");
-  const searchContainer = search.querySelector(".js-search-container");
+  const searchAll = search.querySelector(".js-results-all");
   const searchEmpty = search.querySelector(".js-search-empty");
-  const searchList = search.querySelector(".js-search-list");
+  const searchListBox = search.querySelector(".js-search-list");
+  const searchResetBtn = search.querySelector(".js-reset");
+  const searchResults = search.querySelector(".js-search-results");
+  const common = search.querySelector(".js-common");
+  const filters = search.querySelector(".js-filters");
+  const filterClear = filters.querySelector(".js-filter-clear");
+  const filterNo = filters.querySelector(".js-filter-no");
+  const filterNoText = filters.querySelector(".js-filter-no-text");
+  const filterToggle = search.querySelector(".js-filter-toggle");
+  const filtersListBox = search.querySelector(".js-filters-list");
+  const filtersCheckboxes = filtersListBox.querySelectorAll(
+    "input[type=checkbox]"
+  );
   const inputBox = search.querySelector(".js-search-input");
   const inputLabel = search.querySelector(".js-search-input-label");
   const recommendation = search.querySelector(".js-recommendation");
   const recommendationList = recommendation.querySelector(
     ".js-recommendation-list"
   );
-  const common = search.querySelector(".js-common");
   const submitBtn = search.querySelector(".js-submit");
-  const searchResetBtn = search.querySelector(".js-reset");
-  const searchAll = search.querySelector(".js-results-all");
-  const filters = search.querySelector(".js-filters");
-  const filtersListBox = search.querySelector(".js-filters-list");
-  const filterToggle = search.querySelector(".js-filter-toggle");
-  const filterNoText = filters.querySelector(".js-filter-no-text");
-  const filterNo = filters.querySelector(".js-filter-no");
-  const filterClear = filters.querySelector(".js-filter-clear");
-  const filtersCheckboxes = filtersListBox.querySelectorAll(
-    "input[type=checkbox]"
-  );
-  let filtersList = [];
 
+  // Example data
+  const exampleData = [
+    {
+      label: "Natural Oak",
+      filters: ["kind-of-wood-oak"],
+    },
+    {
+      label: "Oak Light",
+      filters: ["kind-of-wood-oak", "hue-light"],
+    },
+    {
+      label: "Oak medium",
+      filters: ["kind-of-wood-oak", "hue-medium"],
+    },
+    {
+      label: "Oak light gray",
+      filters: ["kind-of-wood-oak", "hue-light", "pattern-gray"],
+    },
+    {
+      label: "Walnut tres",
+      filters: ["kind-of-wood-walnut"],
+    },
+    {
+      label: "Light Walnut",
+      filters: ["kind-of-wood-walnut", "hue-light"],
+    },
+    {
+      label: "Dark Walnut tres",
+      filters: ["kind-of-wood-walnut", "hue-dark"],
+    },
+    {
+      label: "Wood Flooring",
+      filters: [],
+    },
+    {
+      label: "Laminate Flooring",
+      filters: [],
+    },
+    {
+      label: "Caring for Laminated Flooring",
+      filters: [],
+    },
+    {
+      label: "Vinyl Flooring",
+      filters: [],
+    },
+    {
+      label: "Caring for Vinyl Flooring",
+      filters: [],
+    },
+  ];
+
+  // Initial variables
+  let searchList = [];
+  let selectedFilters = [];
+  let userValue = "";
+
+  // Search link to Tarkett search function
   const searchLink = (searchTerm) => {
     return `https://home.tarkett.com/en_EU/search/products?search[body]=${searchTerm}&userQuery=${searchTerm}`;
   };
 
+  // Filters button toggle function
   const toggleShowFilters = () => {
     if (filters.classList.contains("search__filters--active")) {
       return filters.classList.remove("search__filters--active");
@@ -86,90 +93,90 @@ document.addEventListener("DOMContentLoaded", () => {
     return filters.classList.add("search__filters--active");
   };
 
+  // Search dropdown toggle function
   const toggleShowSearch = () => {
     if (inputBox.value.length !== 0) return;
-    if (searchContainer.hidden) {
-      return (searchContainer.hidden = false);
+    if (search.classList.contains("search--active")) {
+      return search.classList.remove("search--active");
     }
-
-    return (searchContainer.hidden = true);
+    return search.classList.add("search--active");
   };
 
   // Function to show recommendations
-  const showRecommendations = (list, userValue) => {
+  const showRecommendations = () => {
     recommendation.hidden = true;
-    if (!list || !userValue) {
-      return (recommendationList.innerHTML = "");
+
+    if (searchList.length === 0 && !userValue) {
+      recommendationList.innerHTML = "";
+      return;
     }
-    const listData = list
+
+    const recommendationsHTML = searchList
       .map((data) => {
         const regex = new RegExp("(" + userValue + ")", "gi");
-        let searchTerm = data.label;
-        searchTerm = searchTerm.replace(regex, "<strong>$1</strong>");
+        const searchTerm = data.label.replace(regex, "<strong>$1</strong>");
         const searchQuery = data.label.toLowerCase();
         const webLink = searchLink(searchQuery);
 
-        if (!filters.classList.contains("search__filters--active"))
+        if (!filters.classList.contains("search__filters--active")) {
           recommendation.hidden = false;
+        }
 
         return `<li class="search__recommendation-list-item">
-          <a target="_blank" href="${webLink}" class="search__recommendation-list-link js-recommendation-item" href="blank"
-            >${searchTerm}</a
-          >`;
+        <a target="_blank" href="${webLink}" class="search__recommendation-list-link js-recommendation-item" href="blank">
+          ${searchTerm}
+        </a>
+      </li>`;
       })
       .join("");
-    return (recommendationList.innerHTML = listData);
+
+    recommendationList.innerHTML = recommendationsHTML;
   };
 
-  // Function to show recommendations
-  const showResults = (list, userValue) => {
-    searchList.hidden = true;
+  // Function to show search results
+  const showResults = () => {
+    searchListBox.hidden = true;
     searchEmpty.hidden = false;
     searchAll.hidden = true;
+    common.hidden = searchList.length !== 0;
 
-    if (!list || !userValue) {
-      return (searchList.innerHTML = "");
+    if (searchList.length === 0 && !userValue) {
+      searchListBox.innerHTML = "";
+      return;
     }
-    const listData = list
+
+    const filteredResults = searchList.filter((data) => {
+      return selectedFilters.every((filter) => data.filters.includes(filter));
+    });
+
+    const searchResultsHTML = filteredResults
       .map((data) => {
         const searchQuery = data.label.toLowerCase();
         const webLink = searchLink(searchQuery);
         searchEmpty.hidden = true;
-        searchList.hidden = false;
+        searchListBox.hidden = false;
         searchAll.hidden = false;
 
-        return `<li class="search__results-list-item" data-filters="${data.filters}">
-                  <a class="search__results-list-link" href="${webLink}" target="_blank">
-                    <picture class="search__results-list-picture">
-                      <source
-                        media="(min-width:1024px)"
-                        srcset="./assets/images/example-image-big.jpg"
-                      />
-                      <img
-                        class="search__results-list-image"
-                        src="./assets/images/example-image-small.jpg"
-                        alt="${userValue}"
-                      />
-                    </picture>
-                    <span class="search__results-list-info">
-                      <span class="search__results-list-item-title"
-                        >${data.label}</span
-                      >
-                      <span class="search__results-list-square-meter"
-                        >SEK 529 /M²</span
-                      >
-                      <span class="search__results-list-square-package"
-                        >SEK 1,334.00 /PACKAGE</span
-                      >
-                    </span>
-                  </a>
-                </li>`;
+        return `<li class="search__results-list-item js-results-item" data-filters="${data.filters}">
+                <a class="search__results-list-link" href="${webLink}" target="_blank">
+                  <picture class="search__results-list-picture">
+                    <source media="(min-width:1024px)" srcset="./assets/images/example-image-big.jpg" />
+                    <img class="search__results-list-image" src="./assets/images/example-image-small.jpg" alt="${userValue}" />
+                  </picture>
+                  <span class="search__results-list-info">
+                    <span class="search__results-list-item-title">${data.label}</span>
+                    <span class="search__results-list-square-meter">SEK 529 /M²</span>
+                    <span class="search__results-list-square-package">SEK 1,334.00 /PACKAGE</span>
+                  </span>
+                </a>
+              </li>`;
       })
       .join("");
-    return (searchList.innerHTML = listData);
+
+    searchListBox.innerHTML = searchResultsHTML;
   };
 
-  const actionFill = (text) => {
+  const formActionLink = (text) => {
     if (!text) {
       searchAll.href = "";
       return (search.action = "");
@@ -182,51 +189,59 @@ document.addEventListener("DOMContentLoaded", () => {
   // Function to select a suggestion
   const select = (element) => {
     const selectData = element.textContent.toLowerCase();
-    actionFill(selectData);
+    formActionLink(selectData);
     inputBox.value = selectData;
     inputLabel.hidden = true;
     search.classList.remove("search--active");
     searchResetBtn.hidden = true;
   };
 
-  inputBox.addEventListener("keyup", (e) => {
-    const userValue = e.target.value;
-    let searchList = [];
+  // Event listeners
 
-    if (userValue) {
-      searchList = recommendations.filter((data) =>
-        data.label.toLowerCase().includes(userValue.toLowerCase())
-      );
-      actionFill(userValue);
-      inputLabel.hidden = false;
+  // Search function for input field based on keypress
+  inputBox.addEventListener("keyup", (e) => {
+    userValue = e.target.value;
+    formActionLink(userValue);
+    inputLabel.hidden = !userValue;
+    searchResetBtn.hidden = !userValue;
+    searchResults.hidden = !userValue;
+
+    if (!!userValue) {
       search.classList.add("search--active");
-      searchResetBtn.hidden = false;
       common.hidden = true;
-      showRecommendations(searchList, userValue);
-      showResults(searchList, userValue);
-      return;
+    } else {
+      common.hidden = filters.classList.contains("search__filters--active");
+      searchEmpty.hidden = true;
     }
 
-    actionFill();
-    inputLabel.hidden = true;
-    common.hidden = false;
-    search.classList.remove("search--active");
-    if (!filters.classList.contains("search__filters--active"))
-      common.hidden = false;
-    searchResetBtn.hidden = true;
+    searchList = userValue
+      ? exampleData.filter((data) =>
+          data.label.toLowerCase().includes(userValue.toLowerCase())
+        )
+      : [];
+
     showRecommendations();
     showResults();
+  });
 
-    return;
+  inputBox.addEventListener("click", toggleShowSearch);
+
+  search.addEventListener("click", (e) => {
+    if (e.target.classList.contains("js-recommendation-item")) {
+      select(e.target);
+    }
   });
 
   searchResetBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    searchContainer.hidden = true;
-    actionFill();
+    searchList = [];
+    selectedFilters = [];
+    userValue = "";
+    formActionLink();
     inputBox.value = "";
     inputLabel.hidden = true;
     search.action = "";
+    common.hidden = false;
     search.classList.remove("search--active");
     searchResetBtn.hidden = true;
     showRecommendations();
@@ -235,38 +250,32 @@ document.addEventListener("DOMContentLoaded", () => {
     toggleShowSearch();
   });
 
-  search.addEventListener("click", (e) => {
-    if (e.target.classList.contains("js-recommendation-item")) {
-      select(e.target);
-    }
-  });
-
-  inputBox.addEventListener("click", toggleShowSearch);
-
   filterToggle.addEventListener("click", toggleShowFilters);
 
   filterClear.addEventListener("click", () => {
-    filtersList = [];
+    selectedFilters = [];
     filterClear.hidden = true;
     filterNoText.hidden = true;
     filtersCheckboxes.forEach((checkbox) => (checkbox.checked = false));
+    showResults();
   });
 
   filtersCheckboxes.forEach((checkbox) => {
     checkbox.addEventListener("change", () => {
-      filtersList = Array.from(filtersCheckboxes)
-        .filter((i) => i.checked)
-        .map((i) => i.value);
+      selectedFilters = Array.from(filtersCheckboxes)
+        .filter((checkbox) => checkbox.checked)
+        .map((checkbox) => checkbox.value);
 
-      if (filtersList.length === 0) {
+      showResults();
+
+      if (selectedFilters.length === 0) {
         filterClear.hidden = true;
         return (filterNoText.hidden = true);
       }
-      filterNo.textContent = filtersList.length;
+
+      filterNo.textContent = selectedFilters.length;
       filterClear.hidden = false;
       filterNoText.hidden = false;
-
-      console.log(filtersList);
 
       return;
     });
